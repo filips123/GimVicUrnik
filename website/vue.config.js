e05d84392cf7e646aac0e38b026180ab234dca0f
Filip Š <filip.stamcar@hotmail.com>
2021-08-21 20:00:06 +0200

    Improve PWA details and shortcuts

--- website/vue.config.js
+++ website/vue.config.js
@@ -25,7 +25,38 @@ module.exports = {
       name: process.env.VUE_APP_TITLE,
       short_name: process.env.VUE_APP_SHORT,
       description: process.env.VUE_APP_DESCRIPTION,
+      categories: process.env.VUE_APP_CATEGORIES.split(','),
       keywords: process.env.VUE_APP_KEYWORDS.split(','),
+
+      shortcuts: [
+        {
+          name: 'Urnik',
+          url: '/timetable',
+          icons: [{
+            src: '/img/shortcuts/timetable.png',
+            type: 'image/png',
+            sizes: '192x192'
+          }]
+        },
+        {
+          name: 'Jedilnik',
+          url: '/menus',
+          icons: [{
+            src: '/img/shortcuts/menus.png',
+            type: 'image/png',
+            sizes: '192x192'
+          }]
+        },
+        {
+          name: 'Okrožnice',
+          url: '/documents',
+          icons: [{
+            src: '/img/shortcuts/documents.png',
+            type: 'image/png',
+            sizes: '192x192'
+          }]
+        }
+      ],
 
       theme_color: '#007300',
       background_color: '#ffffff'
