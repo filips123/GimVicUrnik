a31cdd1122ba87f9e21ce19546171c31657942a2
PetJer <peterjereb3@gmail.com>
2023-09-09 13:30:51 +0200

    Separated parsers to match extension

--- API/gimvicurnik/errors/__init__.py
+++ API/gimvicurnik/errors/__init__.py
@@ -1,6 +1,13 @@
 from .base import GimVicUrnikError
 
 from .config import ConfigError, ConfigParseError, ConfigReadError, ConfigValidationError
-from .eclassroom import ClassroomApiError, ClassroomError, InvalidRecordError, InvalidTokenError
+from .eclassroom import (
+    ClassroomApiError,
+    ClassroomError,
+    InvalidRecordError,
+    InvalidTokenError,
+    SubstitutionsFormatError,
+    LunchScheduleFormatError,
+)
 from .menu import MenuApiError, MenuDateError, MenuFormatError
 from .timetable import TimetableApiError
