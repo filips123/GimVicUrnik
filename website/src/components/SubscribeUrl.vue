d09f2be68918205a8079eba6a321f8d0f1aedd9a
PetJer <peterjereb3@gmail.com>
2024-01-15 21:07:41 +0100

    Subscribe rewrite

---
+++ website/src/components/SubscribeUrl.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  url: string
+}>()
+
+function copyLink() {
+  navigator.clipboard.writeText(props.url)
+}
+
+function openLink() {
+  window.open(props.url, '_blank')
+}
+</script>
+
+<template>
+  <v-row class="pt-2">
+    <h3>{{ label }}</h3>
+  </v-row>
+
+  <v-row>
+    <v-text-field color="green" :value="url" @click="$event.target.select()" />
+    <div class="pt-3">
+      <v-btn variant="text" color="green" class="mx-1" @click="copyLink()" text="Kopiraj" />
+      <v-btn variant="text" color="green" class="mx-1" @click="openLink()" text="Odpri" />
+    </div>
+  </v-row>
+</template>
--- website/src/views/Subscribe.vue
+++ website/src/views/Subscribe.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import { EntityType, useSettingsStore } from '@/stores/settings'
+
+import SubscribeUrl from '@/components/SubscribeUrl.vue'
+
+document.title = import.meta.env.VITE_TITLE + ' - Naročanje'
+
+const api = import.meta.env.VITE_API
+
+const settingsStore = useSettingsStore()
+const { entities, entityType } = settingsStore
+</script>
+
 <template>
-  <h1>SUBSCRIBE</h1>
+  <div class="pt-4 mx-auto" style="max-width: 35rem">
+    <div>
+      <SubscribeUrl label="Okrožnice" :url="`${api}/feed/circulars.atom`" />
+      <SubscribeUrl label="Nadomeščanja" :url="`${api}/feed/substitutions.atom`" />
+      <SubscribeUrl label="Jedilniki" :url="`${api}/feed/menus.atom`" />
+      <SubscribeUrl label="Razporedi kosila" :url="`${api}/feed/schedules.atom`" />
+    </div>
+    <div v-if="entityType === EntityType.Class" class="mt-16">
+      <SubscribeUrl label="Urnik in Nadomeščanja" :url="`${api}/calendar/combined/${entities}`" />
+      <SubscribeUrl label="Urnik" :url="`${api}/calendar/timetable/${entities}`" />
+      <SubscribeUrl label="Nadomeščanja" :url="`${api}/calendar/substitutions/${entities}`" />
+      <SubscribeUrl label="Razporedi kosila" :url="`${api}/calendar/schedules/${entities}`" />
+    </div>
+  </div>
 </template>
